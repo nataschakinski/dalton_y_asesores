96e1fbf6aae5857ecabc860750a1b055bc2775f2
Jonathan Armando Rojas Lopez <jonathanrojaslopez@gmail.com>
2018-11-22 17:49:18 -0500

    adding header animations

--- src/components/atoms/subHeader/SubHeader.js
+++ src/components/atoms/subHeader/SubHeader.js
@@ -1,20 +1,27 @@
 import React from 'react'
+import Typist from 'react-typist';
 import './subHeader.sass'
 
 const subHeaderBackGroundStyle = (image) => {
     const style = {
         background: ` linear-gradient(rgba(0, 0, 0, 0.63), rgba(0, 0, 0, 0.63) ), url(${image})`,
         backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
         backgroundSize: "100% 100%"
     }
     return style;
 }
 
-const SubHeader = ({title, image, text}) => {
+const SubHeader = ({ title, image, text }) => {
     return (
         <section style={subHeaderBackGroundStyle(image)} className="subHeader">
-            <h1 className="subHeader__title">{title}</h1>
-            <p className="subHeader__paragraph">{text}</p>
+            <Typist className="subHeader__title" cursor={{ show: false }}>
+                {title}
+            </Typist>
+            <Typist className="subHeader__paragraph" cursor={{ show: false }}>
+                <Typist.Delay ms={2300} />
+                {text}
+            </Typist>
         </section>
     )
 }
